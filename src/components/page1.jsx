5b02b0fce8ef361c8a6380cc3d2b1e2f89948683
SidAsif <asif050md@gmail.com>
2023-09-22 17:11:59 +0530

    icons-added

--- src/components/page1.jsx
+++ src/components/page1.jsx
@@ -1,16 +1,36 @@
-import "./page1.css"
+import "./page1.css";
 
-export const Page1 = () =>{
-    return (
-        <div id="page1">
-            <h4>OVER 200+ ACTIVE LISTINGS</h4>
-            <h1>Discover the Best Nearby Places & Things</h1>
-            <div id="mypic">
-                <img className="icons" src="src/assets/icons/living.png" alt="living" />
-            </div>
-            <div id="arr">
-                <a href="#page2"><div id="arrow"><i className="ri-arrow-down-line"></i></div></a>
-            </div>
-        </div>
-    )
-}
+export const Page1 = () => {
+  return (
+    <div id="page1">
+      <h4>OVER 200+ ACTIVE LISTINGS</h4>
+      <h1>
+        Discover the Best Nearby Places & <br /> Things
+      </h1>
+      <div id="mypic">
+        <a href="#">
+          <div className="icon-name">
+            <img src="src/assets/icons/living.png" alt="living" />
+          </div>
+        </a>
+
+        <a href="#">
+          <img src="src/assets/icons/food.png" alt="Food" />
+        </a>
+        <a href="#">
+          <img src="src/assets/icons/shopping.png" alt="shopping" />
+        </a>
+        <a href="#">
+          <img src="src/assets/icons/emergency.png" alt="emergency" />
+        </a>
+      </div>
+      <div id="arr">
+        <a href="#page2">
+          <div id="arrow">
+            <i className="ri-arrow-down-line"></i>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
