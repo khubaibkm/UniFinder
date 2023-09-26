3579a3f06107f631758b60c533f80461333666f6
Khubaib <khubaibahmadkm@gmail.com>
2023-09-26 15:19:21 +0530

    scroll-to-top added

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@
 import "./App.css";
 // import Button from '@mui/material/Button';
 import DrawerAppBar from "./components/nav";
+import ScrollToTop from "react-scroll-to-top";
 import { Page1 } from "./components/page1";
 import { Categories } from "./components/categories";
 
 function App() {
   return (
     <>
+    <ScrollToTop smooth style={{borderRadius:'50%', padding:'20px 42px 40px 16px' }}/>
       <div className="app">
         <DrawerAppBar/>
         <Page1/>
--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -3,7 +3,10 @@ import "./categories.css";
 export const Categories = () => {
   return (
     <div id="categories">
-
+      <h1 className="heading">
+        Popular Categories
+      </h1>
+        <h4 className="subheading">CHECK THEM OUT</h4>
     </div>
   );
 };
