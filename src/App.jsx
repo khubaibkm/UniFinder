7b64dc03a1ab44c6567fd1dc984932c2d1c40751
Khubaib <khubaibahmadkm@gmail.com>
2023-10-04 18:52:14 +0530

    all previous changes reverted

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,12 @@ import { Categories } from "./components/categories";
 function App() {
   return (
     <>
-      <ScrollToTop
-        className="scrollToTop"
-        smooth
-        style={{ borderRadius: "50%", padding: "20px 42px 40px 16px" }}
-      />
+    <ScrollToTop className="scrollToTop" smooth style={{borderRadius:'50%', padding:'20px 42px 40px 16px' }}/>
       <div className="app">
-        <DrawerAppBar />
-        <Page1 />
-      </div>
+        <DrawerAppBar/>
+        <Page1/>
+        <Categories/>
+      </div> 
     </>
   );
 }
