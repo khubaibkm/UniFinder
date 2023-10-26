fa6b50aa74399ba8d0e1a4a7720cfd27ebef2a8c
SidAsif <asif050md@gmail.com>
2023-10-26 17:21:56 +0530

    living-component added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Page1 } from "./components/page1";
 import { Categories } from "./components/categories";
 import Review from "./components/review";
 import Footer from "./components/footer";
+// import Living from "./components/Living_components/living";
 function App() {
   return (
     <>
@@ -14,23 +15,23 @@ function App() {
         style={{ borderRadius: "50%", padding: "20px 42px 40px 16px" }}
       />
       <div className="app">
-        <DrawerAppBar/>
+        <DrawerAppBar />
         <div id="home">
-        <Page1/>
+          <Page1 />
         </div>
         <div id="categories">
-        <Categories/>
+          <Categories />
         </div>
         <div id="reviews">
-          <Review/>
+          <Review />
         </div>
         {/* <div id="about us">
 
         </div> */}
-        <div id="contact us">          
+        <div id="contact us">
           <Footer />
         </div>
-      </div> 
+      </div>
     </>
   );
 }
---
+++ src/components/Living_components/living.jsx
@@ -0,0 +1,17 @@
+import "./living.css";
+
+export default function Living() {
+  return (
+    <div className="listings">
+      <div className="list">
+        <p style={{ marginBottom: "13px", fontSize: "13px" }}>
+          CHECK OUT OUR LISTINGS
+        </p>
+        <p style={{ lineHeight: 1.3, fontSize: "30px" }}>
+          Explore the Living <br />
+          Categories.
+        </p>
+      </div>
+    </div>
+  );
+}
