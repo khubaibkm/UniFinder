1a3fa0b06805b79aea82155be18c0d338d905217
Khubaib <khubaibahmadkm@gmail.com>
2023-12-05 20:37:18 +0530

    category images compressed and closed signed images added

--- src/pages/Emergency_components/emergency_data.jsx
+++ src/pages/Emergency_components/emergency_data.jsx
@@ -5,7 +5,7 @@ export const MainData = [
     specs: "Buy any medicine at any time",
     serviceOptions: "Medicines, cosmetics, medical equipments",
     timings: "9:00 am to 11:30 pm",
-    emerPlace_img: "/images/living/image 1.png",
+    emerPlace_img: "/images/under_construction.jpg",
     contactImg: "/images/living/6596115 1.png",
     phone1: "+917275807178",
     map: `https://www.google.com/maps/place/Paras+Boys+Hostel+-+Kursi+Road/@26.9548321,80.9956039,17z/data=!4m14!1m7!3m6!1s0x399959b537a27ce7:0xc3b1179e1579e014!2sParas+Boys+Hostel+-+Kursi+Road!8m2!3d26.9548273!4d80.9981788!16s%2Fg%2F11fn5sp32z!3m5!1s0x399959b537a27ce7:0xc3b1179e1579e014!8m2!3d26.9548273!4d80.9981788!16s%2Fg%2F11fn5sp32z?entry=ttu
@@ -21,7 +21,7 @@ export const MainData = [
     specs: "Buy any medicine at any time",
     serviceOptions: "Medicines, cosmetics, medical equipments",
     timings: "9:00 am to 11:30 pm",
-    emerPlace_img: "/images/living/image 1.png",
+    emerPlace_img: "/images/under_construction.jpg",
     contactImg: "/images/living/6596115 1.png",
     phone1: "+917275807178",
     map: `https://www.google.com/maps/place/Paras+Boys+Hostel+-+Kursi+Road/@26.9548321,80.9956039,17z/data=!4m14!1m7!3m6!1s0x399959b537a27ce7:0xc3b1179e1579e014!2sParas+Boys+Hostel+-+Kursi+Road!8m2!3d26.9548273!4d80.9981788!16s%2Fg%2F11fn5sp32z!3m5!1s0x399959b537a27ce7:0xc3b1179e1579e014!8m2!3d26.9548273!4d80.9981788!16s%2Fg%2F11fn5sp32z?entry=ttu
@@ -37,7 +37,7 @@ export const MainData = [
     specs: "Buy any medicine at any time",
     serviceOptions: "Medicines, cosmetics, medical equipments",
     timings: "9:00 am to 11:30 pm",
-    emerPlace_img: "/images/living/image 1.png",
+    emerPlace_img: "/images/under_construction.jpg",
     phone1: "+917275807178",
     map: `https://www.google.com/maps/place/Paras+Boys+Hostel+-+Kursi+Road/@26.9548321,80.9956039,17z/data=!4m14!1m7!3m6!1s0x399959b537a27ce7:0xc3b1179e1579e014!2sParas+Boys+Hostel+-+Kursi+Road!8m2!3d26.9548273!4d80.9981788!16s%2Fg%2F11fn5sp32z!3m5!1s0x399959b537a27ce7:0xc3b1179e1579e014!8m2!3d26.9548273!4d80.9981788!16s%2Fg%2F11fn5sp32z?entry=ttu
         `,
@@ -53,7 +53,7 @@ export const MainData = [
     specs: "Buy any medicine at any time",
     serviceOptions: "Medicines, cosmetics, medical equipments",
     timings: "9:00 am to 11:30 pm",
-    emerPlace_img: "/images/living/image 1.png",
+    emerPlace_img: "/images/under_construction.jpg",
     phone1: "+917275807178",
     map: `https://www.google.com/maps/place/Paras+Boys+Hostel+-+Kursi+Road/@26.9548321,80.9956039,17z/data=!4m14!1m7!3m6!1s0x399959b537a27ce7:0xc3b1179e1579e014!2sParas+Boys+Hostel+-+Kursi+Road!8m2!3d26.9548273!4d80.9981788!16s%2Fg%2F11fn5sp32z!3m5!1s0x399959b537a27ce7:0xc3b1179e1579e014!8m2!3d26.9548273!4d80.9981788!16s%2Fg%2F11fn5sp32z?entry=ttu
         `,
@@ -69,7 +69,7 @@ export const MainData = [
     specs: "Buy any medicine at any time",
     serviceOptions: "Medicines, cosmetics, medical equipments",
     timings: "9:00 am to 11:30 pm",
-    emerPlace_img: "/images/living/image 1.png",
+    emerPlace_img: "/images/under_construction.jpg",
     phone1: "+917275807178",
     map: `https://www.google.com/maps/place/Paras+Boys+Hostel+-+Kursi+Road/@26.9548321,80.9956039,17z/data=!4m14!1m7!3m6!1s0x399959b537a27ce7:0xc3b1179e1579e014!2sParas+Boys+Hostel+-+Kursi+Road!8m2!3d26.9548273!4d80.9981788!16s%2Fg%2F11fn5sp32z!3m5!1s0x399959b537a27ce7:0xc3b1179e1579e014!8m2!3d26.9548273!4d80.9981788!16s%2Fg%2F11fn5sp32z?entry=ttu
         `,
