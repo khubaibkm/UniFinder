8c932be5e17e87596e8802659b05cb3456b9523d
Khubaib <khubaibahmadkm@gmail.com>
2023-12-06 23:31:50 +0530

    footer links added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,12 @@ export default function Footer() {
         <div className="footer_width about">
           <img className="logo" src="/logo_white.png" alt="logo" />
           <p style={{ lineHeight: 1.5 }}>
-          UniFinder is a web application that helps users discover information about various amenities and services in the local area around Integral university campus. Whether you're a freshman looking for the best places to eat or a first-year student in search of nearby hostels, UniFinder provides you with a single platform to access essential information.
+            UniFinder is a web application that helps users discover information
+            about various amenities and services in the local area around
+            Integral university campus. Whether you're a freshman looking for
+            the best places to eat or a first-year student in search of nearby
+            hostels, UniFinder provides you with a single platform to access
+            essential information.
           </p>
         </div>
 
@@ -15,10 +20,10 @@ export default function Footer() {
           <h2 style={{ width: "230px", color: "white" }}>Quick Links</h2>
           <ul>
             <li>
-              <a href="#">Home</a>
+              <a href="/">Home</a>
             </li>
             <li>
-              <a href="#">About</a>
+              <a href="aboutus">About</a>
             </li>
             <li>
               <a href="#">Service</a>
@@ -43,7 +48,10 @@ export default function Footer() {
                   <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
                 </svg>
               </span>
-              <a href="#"> Instagram</a>
+              <a href="https://www.instagram.com/_._.khubaib._._/">
+                {" "}
+                Instagram
+              </a>
             </li>
             <li>
               <span>
@@ -56,7 +64,7 @@ export default function Footer() {
                   <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
                 </svg>
               </span>
-              <a href="#"> Linkedin</a>
+              <a href="https://www.linkedin.com/in/siddiqui-asif/"> Linkedin</a>
             </li>
             <li>
               <span>
@@ -70,7 +78,10 @@ export default function Footer() {
                   <path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z" />
                 </svg>
               </span>
-              <a href="#"> Facebook</a>
+              <a href="https://www.facebook.com/profile.php?id=100023363987470">
+                {" "}
+                Facebook
+              </a>
             </li>
             <li>
               <span>
@@ -83,7 +94,7 @@ export default function Footer() {
                   <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
                 </svg>
               </span>
-              <a href="#"> Email</a>
+              <a href="mailto:khubaibahmadkm@gmail.com">Email</a>
             </li>
           </ul>
         </div>
