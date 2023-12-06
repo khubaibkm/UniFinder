bfc7889292f2e17e2b2a8706966198043e750deb
Khubaib <khubaibahmadkm@gmail.com>
2023-12-06 13:58:03 +0530

    loader added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,21 +1,47 @@
+// Loader.jsx
+import React from "react";
+import loadingImage from "/logo_black.png"; // Replace with the actual path to your image
+import { css, keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
 
-import { css } from "@emotion/react";
-import { ScaleLoader } from "react-spinners";
+const fadeInOut = keyframes`
+  0% {
+    opacity: 0;
+  }
+  25% {
+    opacity: 1;
+
+  }
+  90% {
+    opacity: 0;
+  }
+`;
+
+const OverrideDiv = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Adjust this to the desired height */
+`;
+
+const Image = styled.img`
+  //width: 150px; /* Adjust this to the desired width */
+  //height: 150px; /* Adjust this to the desired height */
+  animation: ${fadeInOut} 4s linear infinite; /* Adjust the duration for smoother animation */
+`;
+
+const LoaderContainer = styled.div`
+  position: absolute;
+`;
 
 const Loader = () => {
-  const override = css`
-    display: block;
-    margin: 0 auto;
-    border-color: red;
-  `;
-
   return (
-    <ScaleLoader
-      css={override}
-      size={150}
-      color={"#123abc"}
-      loading={true}
-    />
+    <OverrideDiv>
+      <LoaderContainer>
+        <Image src={loadingImage} alt="Loading" />
+      </LoaderContainer>
+    </OverrideDiv>
   );
 };
 
